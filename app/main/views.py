b99a0902ed6eb9f539fd67a22396147827a82fce
nealwaga <waganeal@gmail.com>
2022-05-08 21:51:30 +0300

    updated some files

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,9 @@ from flask import render_template
 from flask import render_template,request,redirect,url_for
 from flask_login import login_required,current_user
 from ..models import Pitches, User
-
 from . import main
 from .. import db
+from .forms import PitchForm
 
 
 @main.route('/')
