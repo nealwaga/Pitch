c56fd80bd926ca8d2b3baf2186b39a2255d0ed3e
nealwaga <waganeal@gmail.com>
2022-05-08 21:26:11 +0300

    updated templates folder

--- app/models.py
+++ app/models.py
@@ -1,11 +1,19 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     author = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    password_hash = db.Column(db.String(255))
     password_secure = db.Column(db.String(255))
     pitches_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
 
