10e2f2d5c37d84944c4574e653241a12c07342ea
nealwaga <waganeal@gmail.com>
2022-05-08 20:58:07 +0300

    updated models file

--- app/models.py
+++ app/models.py
@@ -1,13 +1,25 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
-#...
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     author = db.Column(db.String(255))
+    password_secure = db.Column(db.String(255))
     pitches_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
 
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
 
 
     def __repr__(self):
