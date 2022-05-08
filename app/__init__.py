86caf64d93e524af0031503fd4d86532c09a959e
nealwaga <waganeal@gmail.com>
2022-05-08 20:32:40 +0300

    fixed a bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
-from ensurepip import bootstrap
+#from ensurepip import bootstrap
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
-    bootstrap = Bootstrap()
-    db = SQLAlchemy()
-
+    
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
-from app.models import User,Pitches
+from app.models import User, Pitches
 
 
 app = create_app('development')
@@ -10,7 +10,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Pitches = Pitches)
+    return dict(app = app, db = db, User = User, Pitches = Pitches)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
