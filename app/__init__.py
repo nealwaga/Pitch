6c7e7973cad1c9e7fac108fd699828126b8b1a1f
nealwaga <waganeal@gmail.com>
2022-05-08 18:44:01 +0300

    added the variable in config file

--- app/__init__.py
+++ app/__init__.py
@@ -8,14 +8,17 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
     bootstrap = Bootstrap()
+    db = SQLAlchemy()
 
     app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     #Creating the app configurations
     app.config.from_object(config_options[config_name])
 
     #Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     #Registering the blueprint
     from .main import main as main_blueprint
