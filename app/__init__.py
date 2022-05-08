3cf6c2567588aef991657cc96e47e6a57334bb89
nealwaga <waganeal@gmail.com>
2022-05-08 14:41:14 +0300

    creating a variable

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
-    #Creating the app cinfigurations
+    #Creating the app configurations
     app.config.from_object(config_options[config_name])
 
     #Initializing flask extensions
---
+++ app/main/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, Flask
+
+main = Blueprint ('main', __name__)
+
+from . import views,error
+
