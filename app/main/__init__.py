9d2ee6cbcb207a9b33c79159e169a29ed1a1c4f6
nealwaga <waganeal@gmail.com>
2022-05-08 21:13:30 +0300

    updated index file

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,4 +3,8 @@ from flask import Blueprint, Flask
 main = Blueprint ('main', __name__)
 
 from . import views,error
+from flask_login import LoginManager
 
+#login_manager = LoginManager()
+#login_manager.session_protection = 'strong'
+#login_manager.login_view = 'auth.login'
